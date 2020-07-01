053b8dfe8e136f00ec1bdeef6b0c013e55e8bcc1
Michael Skelton <mike@skelton.onl>
2020-07-01 01:59:43 +0000

    Add CMake ViM template file.

---
+++ templates/skeleton.cmake
@@ -0,0 +1,44 @@
+# Set CMake project properties 
+project(sk3l3tal)
+cmake_minimum_required(VERSION 2.8)
+
+# Set CPP compiler flags
+set(CMAKE_CXX_FLAGS 
+${CMAKE_CXX_FLAGS} 
+"-Wall -pedantic -D_GLIBCXX_CONCEPT_CHECKS -std=gnu++14 -pthread")
+
+# CMake package searches go here
+#find_package (Threads)
+
+# CMake variable defs go here
+set(SOURCES)
+set(INCLUDES)
+
+#set(LIBNAME ${PROJECT_NAME})
+#set(LIBNAME_S "${PROJECT_NAME}_s")
+
+
+# CMake include and lib directory search path additions
+#include_directories(${CMAKE_SOURCE_DIR})
+#link_directories(/usr/lib/x86_64-linux-gnu)
+
+# Include all project subdirectories
+#add_subdirectory(./foo)
+#add_subdirectory(./bar)
+
+# DEBUG messages printed to stdout
+#message("SOURCES => ${SOURCES}")
+#message("INCLUDES => ${INCLUDES}")
+
+# Declare CMake project as a //shared object//
+#add_library(${LIBNAME_S} STATIC ${SOURCES})
+#add_library(${LIBNAME} SHARED ${SOURCES} ${INCLUDES})
+
+# Declare CMake project as a //executable//
+#add_executable(${NAME} ${SRCFILES}) 
+#target_link_libraries(${NAME} sk3l3tal)
+
+### install targets ###
+#install(TARGETS ${LIBNAME_S} ${LIBNAME} DESTINATION dist)
+#install(DIRECTORY "${CMAKE_SOURCE_DIR}/" DESTINATION dist/includes FILES_MATCHING PATTERN "*.h")
+
